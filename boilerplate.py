2eb8f592b9a5b57e7df1cec7872643e49bf60c4a
Meithan West <meithan@gmail.com>
2022-12-02 15:55:05 -0600

    Add boilerplate

---
+++ boilerplate.py
@@ -0,0 +1,32 @@
+# Day XX
+
+import sys
+
+# ==============================================================================
+
+# If no specific input given, default to "day<X>.in"
+if len(sys.argv) == 1:
+  sys.argv.append(sys.argv[0].replace(".py", ".in"))
+
+# Parse input
+with open(sys.argv[1]) as f:
+  for line in f:
+    pass
+
+# ------------------------------------------------------------------------------
+# Part 1
+
+
+
+
+
+print("Part 1:", 0)
+
+# ------------------------------------------------------------------------------
+# Part 2
+
+
+
+
+
+print("Part 2:", 0)
