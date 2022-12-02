03e2605255d34669a9f6e19b1a1e08af392008eb
Meithan West <meithan@gmail.com>
2022-12-02 16:15:28 -0600

    Clean up day 1 solution

--- day01/day01.py
+++ day01/day01.py
@@ -1,6 +1,16 @@
+# Day 01: Calorie Counting
 
+import sys
+
+# ==============================================================================
+
+# If no specific input given, default to "day<X>.in"
+if len(sys.argv) == 1:
+  sys.argv.append(sys.argv[0].replace(".py", ".in"))
+
+# Parse input
 elves = []
-with open("day1.in") as f:
+with open(sys.argv[1]) as f:
   cur_elf = []
   for line in f:
     if line.strip() == "":
@@ -9,10 +19,16 @@ with open("day1.in") as f:
     else:
         cur_elf.append(int(line))
 
+# ------------------------------------------------------------------------------
+# Part 1
+
 totals = [sum(elf) for elf in elves]
 
-print(max(totals))
+print("Part 1:", max(totals))
+
+# ------------------------------------------------------------------------------
+# Part 2
 
 totals.sort(reverse=True)
 
-print(sum(totals[:3]))
+print("Part 2:", sum(totals[:3]))
